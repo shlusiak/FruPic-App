0aacffa4f16cbb9e73364795751f75150c70a8ec
Sascha Hlusiak <mail@saschahlusiak.de>
2012-07-06 21:57:02 +0200

    condense detail dialog

--- src/de/saschahlusiak/frupic/detail/DetailDialog.java
+++ src/de/saschahlusiak/frupic/detail/DetailDialog.java
@@ -6,6 +6,8 @@ import de.saschahlusiak.frupic.R;
 import de.saschahlusiak.frupic.model.Frupic;
 import de.saschahlusiak.frupic.model.FrupicFactory;
 import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
@@ -16,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailDialog extends ArrayAdapter<DetailItem> implements DialogInterface.OnClickListener{
 	Context context;
@@ -33,19 +36,18 @@ public class DetailDialog extends ArrayAdapter<DetailItem> implements DialogInte
 		AlertDialog.Builder builder = new AlertDialog.Builder(ctw);
 		builder.setTitle("Frupic #" + frupic.getId());
 		
-		DetailItem items[] = new DetailItem[6];
+		DetailItem items[] = new DetailItem[5];
 		items[0] = new DetailItem(context.getString(R.string.details_posted_by));
 		items[1] = new DetailItem(context.getString(R.string.details_tags));
 		items[2] = new DetailItem(context.getString(R.string.details_date));
 		items[3] = new DetailItem(context.getString(R.string.details_size));
-		items[4] = new DetailItem(context.getString(R.string.details_filesize));
-		items[5] = new DetailItem("URL");
+		items[4] = new DetailItem("URL");
 		
 		String tags = frupic.getTagsString();
 		items[0].setValue(frupic.getUsername());
 		items[1].setValue((tags != null) ? tags : "---");
 		items[2].setValue(frupic.getDate());
-		items[5].setValue(frupic.getFullUrl());
+		items[4].setValue(frupic.getFullUrl());
 
 		File f = frupic.getCachedFile(factory, false);
 		if (f.exists()) {
@@ -55,32 +57,15 @@ public class DetailDialog extends ArrayAdapter<DetailItem> implements DialogInte
 					frupic, false), options);
 
 			items[3].setValue(String.format(
-					"%d x %d", options.outWidth, options.outHeight));
-			items[4].setValue(f.length() / 1024
-					+ " kb");
-//			findViewById(R.id.cache_now).setVisibility(View.GONE);
+					"%d x %d (%d kb)", options.outWidth, options.outHeight, f.length() / 1024));
 		} else {
 			items[3].setValue(context.getString(R.string.details_not_available));
-			items[4].setValue(context.getString(R.string.details_not_available));
-/*			findViewById(R.id.cache_now).setVisibility(View.VISIBLE);
-			findViewById(R.id.cache_now).setOnClickListener(
-					new OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							showDialog(DIALOG_PROGRESS);
-							fetchTask = new FetchTask(frupic, factory);
-							fetchTask.setActivity(DetailsActivity.this, progressUpdater);
-							fetchTask.execute();
-						}
-					}); */
 		}
 	
 		
-		
-		
-		
 	
 		d = new DetailDialog(context, items, frupic);
+		/* TODO: Specifying an OnClickListener here will dismiss the dialog on select. Do not want! */
 		builder.setAdapter(d, null);
 		builder.setIcon(null);
 		builder.setPositiveButton(android.R.string.ok, new OnClickListener() {
@@ -107,7 +92,14 @@ public class DetailDialog extends ArrayAdapter<DetailItem> implements DialogInte
 
 	@Override
 	public void onClick(DialogInterface dialog, int which) {
-		// TODO Auto-generated method stub
-		
+		/* TODO: this is not called; see setAdapter(...) above */ 
+		switch (which) {
+		case 4:
+			ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+			ClipData clip = ClipData.newPlainText("simple text",frupic.getFullUrl());
+			clipboard.setPrimaryClip(clip);
+			Toast.makeText(context, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
+			break;
+		}
 	}
 }
