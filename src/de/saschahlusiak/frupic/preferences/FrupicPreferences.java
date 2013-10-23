ae51862f30e9ef82c26c0d82641ae0c75c5a38c6
Sascha Hlusiak <mail@saschahlusiak.de>
2013-10-23 14:25:11 +0800

    preferences: actionBar up button sexyness

--- src/de/saschahlusiak/frupic/preferences/FrupicPreferences.java
+++ src/de/saschahlusiak/frupic/preferences/FrupicPreferences.java
@@ -3,25 +3,17 @@ package de.saschahlusiak.frupic.preferences;
 import java.util.List;
 
 import de.saschahlusiak.frupic.R;
-import de.saschahlusiak.frupic.cache.FileCache;
-import de.saschahlusiak.frupic.cache.FileCache.CacheInfo;
-import de.saschahlusiak.frupic.db.FrupicDB;
-import android.app.ProgressDialog;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
 public class FrupicPreferences extends PreferenceActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().setHomeButtonEnabled(true);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 	
 	@Override
@@ -30,4 +22,14 @@ public class FrupicPreferences extends PreferenceActivity {
 		super.onBuildHeaders(target);
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			return(true);
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
