ba8a89e13e17ac27f89ea195ca0762ea58c39cc8
Sascha Hlusiak <mail@saschahlusiak.de>
2025-05-19 21:48:34 +0800

    Fix edge to edge in PreferencesActivity

--- app/src/main/java/de/saschahlusiak/frupic/preferences/FrupicPreferencesActivity.kt
+++ app/src/main/java/de/saschahlusiak/frupic/preferences/FrupicPreferencesActivity.kt
@@ -1,19 +1,34 @@
 package de.saschahlusiak.frupic.preferences
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.squareup.picasso.Picasso
 import de.saschahlusiak.frupic.R
 
 class FrupicPreferencesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.preferences_activity)
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val appbar = findViewById<View>(R.id.appbar)
+        ViewCompat.setOnApplyWindowInsetsListener(appbar) { view, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = bars.top)
+            insets
+        }
 
         val picasso = Picasso.get()
         val stats = picasso.snapshot
--- app/src/main/java/de/saschahlusiak/frupic/preferences/FrupicPreferencesFragment.kt
+++ app/src/main/java/de/saschahlusiak/frupic/preferences/FrupicPreferencesFragment.kt
@@ -1,7 +1,5 @@
 package de.saschahlusiak.frupic.preferences
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -12,43 +10,9 @@ class FrupicPreferencesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
 
-        findPreference<Preference>("donate_bitcoin")?.setOnPreferenceClickListener {
-            onDonateBitcoinClick()
-            true
-        }
-        findPreference<Preference>("donate_litecoin")?.setOnPreferenceClickListener {
-            onDonateLitecoinClick()
-            true
-        }
         findPreference<Preference>("about")?.setOnPreferenceClickListener {
             AboutFragment().show(parentFragmentManager, null)
             true
-        }
-    }
-
-    private fun onDonateBitcoinClick() {
-        val wallet = "bc1qdgm2zvlc6qzqh8qs44wv8l622tfrhvkjqn0fkl"
-
-        try {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("bitcoin:$wallet"))
-            startActivity(intent)
-        }
-        catch (e: Exception) {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.blockchain.com/btc/address/$wallet"))
-            startActivity(intent)
-        }
-    }
-
-    private fun onDonateLitecoinClick() {
-        val wallet = "Lh3YTC7Tv4edEe48kHMbyhgE6BNH22bqBt"
-
-        try {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("litecoin:$wallet"))
-            startActivity(intent)
-        }
-        catch (e: Exception) {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.blockchain.com/ltc/address/$wallet"))
-            startActivity(intent)
         }
     }
 }
