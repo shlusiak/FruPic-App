2c7727f100790f0c68c8dcc658856cf984928168
Sascha Hlusiak <mail@saschahlusiak.de>
2026-04-21 14:28:25 +0800

    Bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,10 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("com.android.tools.build:gradle:9.1.1")
 
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.2.0-2.0.2")
-        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.2.0")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.2")
+        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.2.10")
 
         // https://developers.google.com/android/guides/releases
         classpath("com.google.gms:google-services:4.4.4")
